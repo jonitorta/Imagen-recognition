ae6642cd31b6c3594258924357aca81a60645443
Jonathan_Axel <joni50@live.com.mx>
2022-04-22 08:17:10 -0500

    Agrego funcion para recorrer automaticamete los tweets y guardar las fotos trabajando en bug sobre click.

--- web_scraping.py
+++ web_scraping.py
@@ -24,15 +24,55 @@ my_password = "contraseña123*"
 password = driver.find_element_by_xpath('//input[@name="password"]')
 password.send_keys(my_password)
 password.send_keys(Keys.RETURN)
-sleep(5)
+sleep(3)
 driver.get("https://twitter.com/search?q=%23ReporteCovid19%20%23Guanajuato&src=typed_query&f=live")
+driver.maximize_window()
 sleep(5)
-filtered_elements = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
-date = filtered_elements[0].find_element_by_xpath('.//div[2]/div[2]/div[2]').text
-driver.fullscreen_window()
-images =  filtered_elements[0].find_element_by_xpath('.//div[@data-testid="tweetPhoto"]/img[1]').click()
-sleep(2)
-My_foto = pyautogui.screenshot()
-My_foto.save(r'Imagen_detection\imagenes\file_name.png')
-driver.find_element_by_xpath("//div[@aria-label='Cerrar']").click()
-sleep(10)
+
+
+def get_tweet_data (filtered_element):
+    
+    try:
+        date = filtered_element.find_element_by_xpath('.//div[2]/div[2]/div[2]').text
+    except NoSuchElementException:
+        return
+    sleep(3)
+    try:
+        images =  filtered_element.find_element_by_xpath('.//div[@data-testid="tweetPhoto"]/div[1]').click()
+        sleep(1)
+        My_pic = pyautogui.screenshot()
+        My_pic.save(r'Imagen_detection\imagenes'+"/"+date+".png")
+    except NoSuchElementException:
+        return
+    try:
+        driver.find_element_by_xpath("//div[@aria-label='Cerrar']").click()
+    except NoSuchElementException:
+        return
+    return date
+
+dates = []
+scrolling = True
+last_pos = driver.execute_script("return window.pageYOffset;")
+
+while scrolling:
+    filtered_elements = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
+    for filtered_element in filtered_elements[-2:]:
+        image_info = get_tweet_data(filtered_element)
+        dates.append(image_info)
+
+    scroll_atemp = 0
+    while True :
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        sleep(3)
+        current_pos = driver.execute_script("return window.pageYOffset;")
+        if last_pos == current_pos:
+            scroll_atemp +=1
+            if scroll_atemp >=3:
+                scrolling = False
+                break
+            else: sleep(3)
+        else:
+            last_pos = current_pos
+            break
+        
+
